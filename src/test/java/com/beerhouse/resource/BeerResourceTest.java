0fcbd8a83cec34ffd9b58eeb35295ec06a4fa770
jordano <jordanogoncalves@finchsolucoes.com.br>
2018-03-27 16:51:48 -0300

    Tests
    - Implementa tests no Post

--- src/test/java/com/beerhouse/resource/BeerResourceTest.java
+++ src/test/java/com/beerhouse/resource/BeerResourceTest.java
@@ -3,15 +3,19 @@ package com.beerhouse.resource;
 import com.beerhouse.domain.Beer;
 import com.beerhouse.resources.BeerResource;
 import com.beerhouse.services.BeerService;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
@@ -53,7 +57,7 @@ public class BeerResourceTest {
     }
 
     @Test
-    public void createStudentCourse() throws Exception {
+    public void createBeer() throws Exception {
         Beer beer = new Beer();
         beer.setId(1);
         beer.setName("Beer1");
@@ -63,20 +67,20 @@ public class BeerResourceTest {
 
         Mockito.when(newBeer).thenReturn(Optional.of(beer));
 
-//        RequestBuilder requestBuilder = MockMvcRequestBuilders
-//                .post("/students/Student1/courses")
-//                .accept(MediaType.APPLICATION_JSON).content(exampleCourseJson)
-//                .contentType(MediaType.APPLICATION_JSON);
-//
-//        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
-//
-//        MockHttpServletResponse response = result.getResponse();
-//
-//        assertEquals(HttpStatus.CREATED.value(), response.getStatus());
-//
-//        assertEquals("http://localhost/students/Student1/courses/1",
-//                response.getHeader(HttpHeaders.LOCATION));
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .post("/beers")
+                .accept(MediaType.APPLICATION_JSON)
+                .content("    {\n" +
+                        "        \"id\": 1,\n" +
+                        "        \"name\": \"Jordano's Beer\"\n" +
+                        "    }")
+                .contentType(MediaType.APPLICATION_JSON);
 
+        MvcResult result = mvc.perform(requestBuilder).andReturn();
+
+        MockHttpServletResponse response = result.getResponse();
+
+        Assert.assertEquals(HttpStatus.CREATED.value(), response.getStatus());
     }
 
 }

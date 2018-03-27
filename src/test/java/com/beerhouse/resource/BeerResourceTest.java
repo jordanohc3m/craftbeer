e6be12ce9944a816674fd6db5d8fc8ac20ef1977
jordano <jordanogoncalves@finchsolucoes.com.br>
2018-03-27 13:58:38 -0300

    Tests
    - Implementa tests no Post

--- src/test/java/com/beerhouse/resource/BeerResourceTest.java
+++ src/test/java/com/beerhouse/resource/BeerResourceTest.java
@@ -5,12 +5,15 @@ import com.beerhouse.resources.BeerResource;
 import com.beerhouse.services.BeerService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.util.Optional;
 
@@ -47,6 +50,33 @@ public class BeerResourceTest {
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("name", is(beer.getName())));
+    }
+
+    @Test
+    public void createStudentCourse() throws Exception {
+        Beer beer = new Beer();
+        beer.setId(1);
+        beer.setName("Beer1");
+
+        beerService.create(Mockito.any(Beer.class));
+        Optional<Beer> newBeer = beerService.findOne(beer.getId());
+
+        Mockito.when(newBeer).thenReturn(Optional.of(beer));
+
+//        RequestBuilder requestBuilder = MockMvcRequestBuilders
+//                .post("/students/Student1/courses")
+//                .accept(MediaType.APPLICATION_JSON).content(exampleCourseJson)
+//                .contentType(MediaType.APPLICATION_JSON);
+//
+//        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+//
+//        MockHttpServletResponse response = result.getResponse();
+//
+//        assertEquals(HttpStatus.CREATED.value(), response.getStatus());
+//
+//        assertEquals("http://localhost/students/Student1/courses/1",
+//                response.getHeader(HttpHeaders.LOCATION));
+
     }
 
 }
